daea9b80fa63597670c068f961d8815a2427b29f
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2023-09-19 16:55:25 -0600

    Created main game scene

---
+++ Assets/Scripts/Player/FlashlightBehavior.cs
@@ -0,0 +1,31 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightBehavior : MonoBehaviour
+{
+    private Light flLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flLight = GetComponent<Light>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            flLight.enabled = !flLight.enabled;
+        }
+
+        // Button Y
+        if (Input.GetKeyDown(KeyCode.Joystick1Button3))
+        {
+            flLight.enabled = !flLight.enabled;
+        }
+    }
+}
