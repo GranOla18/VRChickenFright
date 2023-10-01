b562f9f3e550ca8f3db28a83de0fe248937b782d
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2023-10-01 15:22:56 -0600

    Added Laser in Gun with Line Renderer

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -2,22 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(LineRenderer))]
 public class GunShoot : MonoBehaviour
 {
-    RaycastHit choque;
-    Ray rayo;
+    private RaycastHit choque;
+    private Ray rayo;
     public Vector3 offset;
+
+    [SerializeField] private LineRenderer lineRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     private void Update()
     {
         rayo.origin = transform.position + offset;
         rayo.direction = transform.forward;
+        lineRenderer.SetPosition(0, transform.position);
 
         Debug.DrawRay(rayo.origin, rayo.direction * 10, Color.blue);
 
@@ -35,19 +39,18 @@ public class GunShoot : MonoBehaviour
             }
         }
 
-        //if (Input.GetKeyDown(KeyCode.E))
-        //{
-        //    if (Physics.Raycast(rayo, out choque))
-        //    {
-        //        Debug.Log(choque.collider.name);
+        else if (Input.GetKeyDown(KeyCode.Joystick2Button5))
+        {
+            lineRenderer.SetPosition(1, transform.forward * 20);
+            lineRenderer.startWidth = 0.05f;
+            lineRenderer.endWidth = 0.05f;
+        }
 
-        //        if (choque.collider.gameObject.layer == 7)
-        //        {
-        //            choque.collider.gameObject.GetComponent<IDamage>().Damage();
-        //        }
-
-        //    }
-        //}
+        else if (Input.GetKeyDown(KeyCode.Joystick2Button5))
+        {
+            lineRenderer.startWidth = 0f;
+            lineRenderer.endWidth = 0f;
+        }
     }
 }
     
