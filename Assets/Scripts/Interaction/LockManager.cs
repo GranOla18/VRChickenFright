9b60e29d4467fda9b0daf44c5e015bb163a89219
DPD-debug <diegoreas@hotmail.com>
2023-10-02 00:26:56 -0600

    Ammo boxes, belt, lighting, map placement
    Ammo boxes have been added, a belt to store items, a lock for the door and the lightning has been adjusted for the lashlight and ambient light

---
+++ Assets/Scripts/Interaction/LockManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class LockManager : MonoBehaviour
+{
+    XRSocketInteractor socket;
+    public GameObject barrier;
+
+    void Start()
+    {
+        socket = GetComponent<XRSocketInteractor>();
+    }
+
+    public void UnlockDoor()
+    {
+
+        IXRSelectInteractable objName = socket.GetOldestInteractableSelected();
+
+        Debug.Log(objName.transform.name + " in socket of " + transform.name);
+        GameObject objNameCol = objName.transform.gameObject;
+        if (objNameCol.tag == "Key" )
+        {
+            Destroy(barrier);
+            Destroy(objNameCol);
+
+        }
+
+    }
+
+}
